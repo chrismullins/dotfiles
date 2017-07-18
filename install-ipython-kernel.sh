c74b3137948961e7fa49b37df22bfe181d3d6fe9
Christopher Mullins <christopherrmullins@gmail.com>
2017-07-18 09:33:52 -0400

    Add command to install ipython kernel

---
+++ install-ipython-kernel.sh
@@ -0,0 +1,4 @@
+# from https://ipython.readthedocs.io/en/latest/install/kernel_install.html
+
+source activate other-env
+python -m ipykernel install --user --name other-env --display-name "Python (other-env)"
