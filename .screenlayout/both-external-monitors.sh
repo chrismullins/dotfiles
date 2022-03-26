66245564cd34ca915d6fc12d9227262ae0e94473
Chris Mullins <crmullin@fastmail.com>
2022-03-26 11:53:42 -0700

    Thinkpad stuff

---
+++ .screenlayout/both-external-monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1-0 --mode 1920x1200 --pos 6144x0 --rotate left --output DP-1-1 --mode 3840x2160 --pos 2304x0 --rotate normal --output HDMI-1-0 --off
